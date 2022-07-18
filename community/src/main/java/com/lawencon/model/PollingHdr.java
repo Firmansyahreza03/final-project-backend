9f5c3f4d9dc282bea074a4274a6bb6d850059266
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2022-07-18 17:36:49 +0700

    add PollingHdr

---
+++ community/src/main/java/com/lawencon/model/PollingHdr.java
@@ -0,0 +1,24 @@
+package com.lawencon.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.lawencon.base.BaseEntity;
+
+@Entity
+@Table(name = "comm_polling_hdr")
+public class PollingHdr extends BaseEntity {
+	private static final long serialVersionUID = 1171432366136273732L;
+
+	@Column(name = "polling_name", columnDefinition = "TEXT")
+	private String pollingName;
+
+	public String getPollingName() {
+		return pollingName;
+	}
+
+	public void setPollingName(String pollingName) {
+		this.pollingName = pollingName;
+	}
+}

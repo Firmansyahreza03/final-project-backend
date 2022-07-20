0d8c5c0f815826b61b382fa0867e202d907d4c75
Muhammad Reza Firmansyah <fmuhammadreza3@gmail.com>
2022-07-20 11:25:02 +0700

    add pojo pollinghdr

---
+++ community/src/main/java/com/lawencon/community/pojo/pollinghdr/PojoFindAllPollingHdrRes.java
@@ -0,0 +1,24 @@
+package com.lawencon.community.pojo.pollinghdr;
+
+public class PojoFindAllPollingHdrRes {
+
+	private PojoPollingHdrData data;
+	private String message;
+
+	public PojoPollingHdrData getData() {
+		return data;
+	}
+
+	public void setData(PojoPollingHdrData data) {
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ community/src/main/java/com/lawencon/community/pojo/pollinghdr/PojoPollingHdrData.java
@@ -0,0 +1,33 @@
+package com.lawencon.community.pojo.pollinghdr;
+
+public class PojoPollingHdrData {
+
+	private String id;
+	private String pollingName;
+	private Boolean isActive;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPollingName() {
+		return pollingName;
+	}
+
+	public void setPollingName(String pollingName) {
+		this.pollingName = pollingName;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}

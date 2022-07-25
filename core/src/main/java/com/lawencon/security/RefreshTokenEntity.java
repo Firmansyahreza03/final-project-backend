9874aaa06cf44efdba0498cde2d2968fe2eda440
Muhammad Reza Firmansyah <fmuhammadreza3@gmail.com>
2022-07-25 13:22:14 +0700

    add update from base

---
+++ core/src/main/java/com/lawencon/security/RefreshTokenEntity.java
@@ -0,0 +1,38 @@
+package com.lawencon.security;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.lawencon.base.BaseEntity;
+
+@Entity
+@Table(name = "refresh_token")
+public class RefreshTokenEntity extends BaseEntity {
+
+	private static final long serialVersionUID = 4663269825742405548L;
+
+	@Column(columnDefinition = "TEXT", nullable = false, unique = true)
+	private String token;
+
+	@Column(nullable = false)
+	private LocalDateTime expiredDate;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public LocalDateTime getExpiredDate() {
+		return expiredDate;
+	}
+
+	public void setExpiredDate(LocalDateTime expiredDate) {
+		this.expiredDate = expiredDate;
+	}
+}
--- core/src/main/java/com/lawencon/util/JwtUtil.java
+++ core/src/main/java/com/lawencon/util/JwtUtil.java
@@ -4,10 +4,13 @@ import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.stereotype.Component;
+
+import com.lawencon.security.RefreshTokenEntity;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
@@ -19,34 +22,38 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtil {
 
 	private SecretKey secretKey;
-	
+	private final LocalDateTime expiredDateRefreshToken = LocalDateTime.now().plusWeeks(1);
+
 	public JwtUtil() {
 		secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	}
-	
+
 	public String generateToken(Map<String, Object> claims, Duration duration) {
 		LocalDateTime expiredDate = LocalDateTime.now().plusSeconds(duration.getSeconds());
+		
 		JwtBuilder jwtBuilder = Jwts.builder()
 				.signWith(secretKey)
 				.setExpiration(Timestamp.valueOf(expiredDate))
 				.setClaims(claims);
-		
+
 		return jwtBuilder.compact();
 	}
-	
+
+	public RefreshTokenEntity generateRefreshToken() {
+		String uuid = UUID.randomUUID().toString();
+		RefreshTokenEntity refreshToken = new RefreshTokenEntity();
+		refreshToken.setToken(uuid);
+		refreshToken.setExpiredDate(expiredDateRefreshToken);
+		return refreshToken;
+	}
+
 	public Claims getClaims(String token) {
-		Claims result = Jwts.parserBuilder()
-				.setSigningKey(secretKey)
-				.build()
-				.parseClaimsJws(token)
-				.getBody();
-		
+		Claims result = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+
 		return result;
 	}
-	
+
 	public enum ClaimKey {
 		ID, ROLE
 	}
 }
-
-

9a4a336404432ff8b8b05f3a4e7d6f2618bfbe75
Muhammad Reza Firmansyah <fmuhammadreza3@gmail.com>
2022-08-15 10:48:23 +0700

    update project

--- community/src/main/java/com/lawencon/community/dao/RefreshTokenDao.java
+++ community/src/main/java/com/lawencon/community/dao/RefreshTokenDao.java
@@ -7,5 +7,23 @@ import com.lawencon.security.RefreshTokenEntity;
 
 @Repository
 public class RefreshTokenDao extends AbstractJpaDao<RefreshTokenEntity>{
-
+	public String findRefreshTokenIdFromAccessToken(String accessToken) throws Exception{
+		StringBuilder sql = new StringBuilder()
+				.append(" SELECT rt.id FROM refresh_token rt ")
+				.append(" WHERE rt.access_token = :accessToken ");
+		
+		String res = null;
+		try {
+			Object result = createNativeQuery(sql.toString())
+					.setParameter("accessToken", accessToken)
+					.getSingleResult();
+			
+			if(result != null) {
+				res = result.toString();
+			}
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return res;
+	}
 }
